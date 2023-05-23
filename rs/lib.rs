5e85606e1924428f1c527bcf240192f80355220a
c.lamboo <casperlamboo@gmail.com>
2023-05-23 17:43:19 +0200

    Remove redudent namespace nesting in rust packagin

--- rs/lib.rs
+++ rs/lib.rs
@@ -1,54 +1,50 @@
-pub mod cura {
-    pub mod plugins {
+pub mod v0 {
+    tonic::include_proto!("cura.plugins.v0");
+}
+pub mod slots {
+    pub mod overhang_areas {
         pub mod v0 {
-            tonic::include_proto!("cura.plugins.v0");
+            tonic::include_proto!("cura.plugins.slots.overhang_areas.v0");
         }
-        pub mod slots {
-            pub mod overhang_areas {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.overhang_areas.v0");
-                }
-            }
-            pub mod comb {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.comb.v0");
-                }
-            }
-            pub mod platform_adhesion {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.platform_adhesion.v0");
-                }
-            }
-            pub mod postprocess {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.postprocess.v0");
-                }
-            }
-            pub mod skin {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.skin.v0");
-                }
-            }
-            pub mod slice {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.slice.v0");
-                }
-            }
-            pub mod simplify {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.simplify.v0");
-                }
-            }
-            pub mod infill {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.infill.v0");
-                }
-            }
-            pub mod wall_tool_paths {
-                pub mod v0 {
-                    tonic::include_proto!("cura.plugins.slots.wall_toolpaths.v0");
-                }
-            }
+    }
+    pub mod comb {
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.slots.comb.v0");
+        }
+    }
+    pub mod platform_adhesion {
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.slots.platform_adhesion.v0");
+        }
+    }
+    pub mod postprocess {
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.slots.postprocess.v0");
+        }
+    }
+    pub mod skin {
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.slots.skin.v0");
+        }
+    }
+    pub mod slice {
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.slots.slice.v0");
+        }
+    }
+    pub mod simplify {
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.slots.simplify.v0");
+        }
+    }
+    pub mod infill {
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.slots.infill.v0");
+        }
+    }
+    pub mod wall_tool_paths {
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.slots.wall_toolpaths.v0");
         }
     }
 }

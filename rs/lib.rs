47aaa00c002a2837afe1e26b9bcbe987b6b86c20
c.lamboo <casperlamboo@gmail.com>
2023-05-23 16:58:01 +0200

    Move namespaces to v0

--- rs/lib.rs
+++ rs/lib.rs
@@ -1,52 +1,52 @@
 pub mod cura {
     pub mod plugins {
-        pub mod v1 {
-            tonic::include_proto!("cura.plugins.v1");
+        pub mod v0 {
+            tonic::include_proto!("cura.plugins.v0");
         }
         pub mod slots {
             pub mod overhang_areas {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.overhang_areas.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.overhang_areas.v0");
                 }
             }
             pub mod comb {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.comb.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.comb.v0");
                 }
             }
             pub mod platform_adhesion {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.platform_adhesion.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.platform_adhesion.v0");
                 }
             }
             pub mod postprocess {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.postprocess.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.postprocess.v0");
                 }
             }
             pub mod skin {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.skin.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.skin.v0");
                 }
             }
             pub mod slice {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.slice.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.slice.v0");
                 }
             }
             pub mod simplify {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.simplify.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.simplify.v0");
                 }
             }
             pub mod infill {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.infill.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.infill.v0");
                 }
             }
             pub mod wall_tool_paths {
-                pub mod v1 {
-                    tonic::include_proto!("cura.plugins.slots.wall_toolpaths.v1");
+                pub mod v0 {
+                    tonic::include_proto!("cura.plugins.slots.wall_toolpaths.v0");
                 }
             }
         }

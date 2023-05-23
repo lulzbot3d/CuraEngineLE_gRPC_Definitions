b175f6c32fa1eb166662520a419f4240ad242bd8
c.lamboo <casperlamboo@gmail.com>
2023-05-23 16:50:38 +0200

    Use glob to find all .proto files in rust

--- rs/build.rs
+++ rs/build.rs
@@ -1,31 +1,18 @@
+use glob::glob;
 use std::io::Result;
 
 fn main() -> Result<()> {
+    let proto_files = glob("**/*.proto")
+        .expect("Failed to read glob pattern")
+        .into_iter()
+        .map(|p| p.ok().expect("Failed to read glob pattern"))
+        .collect::<Vec<_>>();
+
     // Compile the proto files
     tonic_build::configure()
         .build_server(true)
         .build_client(false)
-        .compile(
-            &[
-                "cura/plugins/overhang_areas/v1/overhang_areas.proto",
-                "cura/plugins/comb/v1/comb.proto",
-                "cura/plugins/infill/v1/infill.proto",
-                "cura/plugins/platform_adhesion/v1/platform_adhesion.proto",
-                "cura/plugins/postprocess/v1/postprocess.proto",
-                "cura/plugins/simplify/v1/simplify.proto",
-                "cura/plugins/skin/v1/skin.proto",
-                "cura/plugins/slice/v1/slice.proto",
-                "cura/plugins/v1/broadcast_slots.proto",
-                "cura/plugins/v1/cura.proto",
-                "cura/plugins/v1/layers.proto",
-                "cura/plugins/v1/mesh.proto",
-                "cura/plugins/v1/polygons.proto",
-                "cura/plugins/v1/slot_id.proto",
-                "cura/plugins/v1/toolpaths.proto",
-                "cura/plugins/wall_toolpaths/v1/wall_toolpaths.proto",
-            ],
-            &["./"],
-        )?;
+        .compile(&proto_files, &["./"])?;
 
     Ok(())
 }
